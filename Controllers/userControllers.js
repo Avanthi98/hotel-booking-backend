aaa39a7da047f3c28a1a01ce57dcc31eedea7637
Avanthi98 <avanthilakmali41@gmail.com>
2024-10-16 18:11:47 +0530

    Implementing .env

--- Controllers/userControllers.js
+++ Controllers/userControllers.js
@@ -1,6 +1,8 @@
 import User from "../Models/userModel.js"
 import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt"
+import dotenv from "dotenv"
+dotenv.config()
 
 //Function for Getting user list
 export function getUsers(req,res){
@@ -64,7 +66,7 @@ export function loginUser(req, res) {
             };
 
             // Sign the token with the payload and secret
-            const token = jwt.sign(payload, "secret", { expiresIn: "1h" });
+            const token = jwt.sign(payload,process.env.JWT_KEY, { expiresIn: "1h" });
 
             res.status(200).json({
                 message: "User logged in successfully",
