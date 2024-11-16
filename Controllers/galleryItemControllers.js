04a5da0ef1a6925da57be30de9ea36dd8f275f3c
Avanthi98 <avanthilakmali41@gmail.com>
2024-11-16 14:59:03 +0530

    Backend Editing

--- Controllers/galleryItemControllers.js
+++ Controllers/galleryItemControllers.js
@@ -17,42 +17,40 @@ export function createGalleryItems(req, res) {
     return;
   }
 
-  const startingId=100;
-    GalleryItem.countDocuments({}).then(
-        (count)=>{
-           const newId = startingId + count + 1; //countDocuments() function will return the count of database records
-            //So count variable will return the count
+  const startingId = 100;
+  GalleryItem.countDocuments({})
+    .then((count) => {
+      const newId = startingId + count + 1; //countDocuments() function will return the count of database records
+      //So count variable will return the count
 
-            const newGalleryItem=new GalleryItem({
-                eventId:newId,
-                name:req.body.name,
-                image:req.body.image,
-                description:req.body.description
-            })
+      const newGalleryItem = new GalleryItem({
+        eventId: newId,
+        name: req.body.name,
+        image: req.body.image,
+        description: req.body.description,
+      });
 
-            newGalleryItem.save().then(
-                (result)=>{
-                    res.json({
-                        message:"Galery Event created successfully",
-                        result:result
-                    })
-                }
-            ).catch(
-                (err)=>{
-                    res.json({
-                        message:"Gallery Event creation failed",
-                        error:err
-                    })
-                }
-            )
-        }
-        ).catch(
-            ()=>{
-                res.json({
-                    message:"Gallery event creation failed due to database connection failure"
-                })
-            }
-        )
+      newGalleryItem
+        .save()
+        .then((result) => {
+          res.json({
+            message: "Galery Event created successfully",
+            result: result,
+          });
+        })
+        .catch((err) => {
+          res.json({
+            message: "Gallery Event creation failed",
+            error: err,
+          });
+        });
+    })
+    .catch(() => {
+      res.json({
+        message:
+          "Gallery event creation failed due to database connection failure",
+      });
+    });
 }
 
 //viewing gallery items
@@ -72,7 +70,7 @@ export function getGalletyItemById(req, res) {
     });
     return;
   }
-  
+
   const eventId = req.params.eventId;
   GalleryItem.findOne({ eventId: eventId })
     .then((result) => {
@@ -110,26 +108,23 @@ export function deleteGalleryItems(req, res) {
     });
 }
 //Update gallery Item
-export function updateGalleryItem(req,res){
-  if(!isAdminValid(req)){
+export function updateGalleryItem(req, res) {
+  if (!isAdminValid(req)) {
     res.json({
-      message:"Forbidden!"
-    })
+      message: "Forbidden!",
+    });
     return;
   }
-  const id=req.params.eventId;
-  GalleryItem.updateOne({eventId:id},req.body).then(
-    ()=>{
-        res.json({
-            message:"Gallery Event updated successfully"
-        })
-    }
-).catch(
-    ()=>{
-        res.json({
-            message:"Failed to update the gallery event"
-        })
-    }
-)
-
+  const id = req.params.eventId;
+  GalleryItem.updateOne({ eventId: id }, req.body)
+    .then(() => {
+      res.json({
+        message: "Gallery Event updated successfully",
+      });
+    })
+    .catch(() => {
+      res.json({
+        message: "Failed to update the gallery event",
+      });
+    });
 }
--- Routes/galleryItemsRoute.js
+++ Routes/galleryItemsRoute.js
@@ -1,15 +1,20 @@
 import express from "express";
 
-import { createGalleryItems,deleteGalleryItems,getGalleryItems, getGalletyItemById, updateGalleryItem } from "../Controllers/galleryItemControllers.js";
+import {
+  createGalleryItems,
+  deleteGalleryItems,
+  getGalleryItems,
+  getGalletyItemById,
+  updateGalleryItem,
+} from "../Controllers/galleryItemControllers.js";
 
 //Creating galleryItem Router
-const galleryItemRouter=express.Router();
+const galleryItemRouter = express.Router();
 
-galleryItemRouter.post("/",createGalleryItems);
-galleryItemRouter.get("/",getGalleryItems);
-galleryItemRouter.get("/:eventId",getGalletyItemById);
-galleryItemRouter.delete("/:eventId",deleteGalleryItems);
-galleryItemRouter.put("/:eventId",updateGalleryItem)
+galleryItemRouter.post("/", createGalleryItems);
+galleryItemRouter.get("/", getGalleryItems);
+galleryItemRouter.get("/:eventId", getGalletyItemById);
+galleryItemRouter.delete("/:eventId", deleteGalleryItems);
+galleryItemRouter.put("/:eventId", updateGalleryItem);
 
 export default galleryItemRouter;
-
